1fe3b9a007d6d878ddbfb29bbe73392aadb8c33c
Karol <siluchkarol@gmail.com>
2025-06-20 14:17:48 +0200

    better insertion

--- game/AABBtree/AABBtree.py
+++ game/AABBtree/AABBtree.py
@@ -11,12 +11,37 @@ class AABBTree:
     def instert(self, tile: Tile) -> None:
         self.x += 1
         if self._head is None:
-            self._head = AABBLeaf(0, tile)
+            self._head = AABBLeaf(tile)
         elif self._head.leaf:
-            self._head = AABBContainer(None, 0, self._head, AABBLeaf(0, tile))
+            self._head = AABBContainer(None, self._head, AABBLeaf(tile))
         else:
             self._head = self._head.insert(tile) or self._head
+            self.rebelance()
         print(self.x)
+
+    def rebelance(self) -> None:
+        node = self._head
+        if node.balanced():
+            print('the tree is balanced')
+            return
+        print('the tree is not balanced')
+        while not node._node1.balanced():
+            node = node._node1
+            print('skipped layer!')
+        print('layer to balance: ', node.layer)
+        print('layer of node1: ', node._node1.layer)
+        print('layer of node2: ', node._node2.layer)
+        node.rebalance()
+        if node.balanced():
+            print('the tree is now balanced')
+
+        # node1 = node._node1._node1
+        # node1.set_parent(node)
+        # node2 = node._node2
+        # node2.set_parent(node._node1)
+
+        # node._node2 = node1
+        # node._node1._node1 = node2
 
     def AABBcollision(self, rect: pygame.FRect) -> list[Tile]:
         result = []
@@ -51,9 +76,6 @@ class AABBNode(ABC):
     def set_parent(self, node):
         self._parent = node
 
-    def set_new_layer(self, layer: int):
-        self._layer = layer
-
     @abstractmethod
     def get_rect(self) -> pygame.FRect: ...
 
@@ -65,9 +87,9 @@ class AABBNode(ABC):
 
 
 class AABBLeaf(AABBNode):
-    def __init__(self, layer, tile):
+    def __init__(self, tile):
         self._tile: Tile = tile
-        super().__init__(True, layer, None, self.get_rect())
+        super().__init__(True, 0, None, self.get_rect())
 
     def get_rect(self) -> pygame.FRect:
         return self._tile.rect
@@ -78,21 +100,19 @@ class AABBLeaf(AABBNode):
             result.append(self._tile)
 
     def print(self, display: pygame.Surface) -> None:
-        print(self._layer)
         pygame.draw.rect(display, 'blue', self._rect, 2)
 
 
 class AABBContainer(AABBNode):
-    def __init__(self, parent: AABBNode, layer: int, node1: AABBNode, node2: AABBNode):
+    def __init__(self, parent: AABBNode, node1: AABBNode, node2: AABBNode):
         self._node1: AABBLeaf | AABBContainer = node1
-        self._node1.set_parent(self)
-        self._node1.set_new_layer(layer + 1)
-
         self._node2: AABBLeaf | AABBContainer = node2
-        self._node2.set_parent(self)
-        self._node2.set_new_layer(layer + 1)
 
-        super().__init__(False, layer, parent, self.get_rect())
+        self._node1.set_parent(self)
+        self._node2.set_parent(self)
+
+        super().__init__(False, max(self._node1.layer, self._node2.layer) + 1, parent, self.get_rect())
+        self.set_new_layer()
 
     @staticmethod
     def get_nodes_rect(node1: AABBNode | Tile, node2: AABBNode | Tile) -> pygame.FRect:
@@ -102,17 +122,30 @@ class AABBContainer(AABBNode):
     def area(rect: pygame.FRect) -> int:
         return rect.width * rect.height
 
-    def set_new_layer(self, layer):
-        super().set_new_layer(layer)
-        self._node1.set_new_layer(layer)
-        self._node1.set_new_layer(layer)
+    def rebalance(self):
+        self._node2, self._node1._node1 = self._node1._node1, self._node2
+        self._node2.set_parent(self)
+        self._node1._node1.set_parent(self._node1)
+        self._node1._layer -= 1
+        self.update_rect()
+        self._node1.update_rect()
+        self.set_new_layer()
 
-    def update_rect(self, rect: pygame.FRect):
-        self._rect = self._rect.union(rect)
+    def set_new_layer(self):
+        node = self
+        while node._parent is not None:
+            node._parent._layer = max(node._parent.layer, node.layer + 1)
+            node = node._parent
+
+    def update_rect(self):
+        if self._node2.layer > self._node1.layer:
+            self._node1, self._node2 = self._node2, self._node1
+
+        self._rect = self.get_rect()
         if self._parent is not None:
-            self._parent.update_rect(self._rect)
+            self._parent.update_rect()
 
-    def get_rect(self) -> None:
+    def get_rect(self) -> pygame.FRect:
         return self.get_nodes_rect(self._node1, self._node2)
 
     def insert(self, tile: Tile) -> AABBNode | None:
@@ -121,30 +154,29 @@ class AABBContainer(AABBNode):
 
         if not self._node1.leaf and self._node1.rect.contains(tile.rect):
             self._node1 = self._node1.insert(tile) or self._node1
-            self.update_rect(self._node1.rect)
 
         elif not self._node2.leaf and self._node2.rect.contains(tile.rect):
             self._node2 = self._node2.insert(tile) or self._node2
-            self.update_rect(self._node2.rect)
 
         elif rect1_area > self.area(self._rect) and rect2_area > self.area(self._rect):
-            return AABBContainer(self._parent, self._layer, self, AABBLeaf(self._layer, tile))
+            return AABBContainer(self._parent, self, AABBLeaf(tile))
 
         elif rect1_area < rect2_area:
             if self._node1.leaf:
-                self._node1 = AABBContainer(self, self._layer + 1, self._node1, AABBLeaf(self._layer + 1, tile))
+                self._node1 = AABBContainer(self, self._node1, AABBLeaf(tile))
             else:
                 self._node1 = self._node1.insert(tile) or self._node1
-            self.update_rect(self._node1.rect)
         else:
             if self._node2.leaf:
-                self._node2 = AABBContainer(self, self._layer + 1, self._node2, AABBLeaf(self._layer + 1, tile))
+                self._node2 = AABBContainer(self, self._node2, AABBLeaf(tile))
             else:
                 self._node2 = self._node2.insert(tile) or self._node2
-            self.update_rect(self._node2.rect)
+        self.update_rect()
 
-    # def restructure(self):
-    #     if self
+    def balanced(self) -> bool:
+        if self._node1.leaf and self._node2.leaf:
+            return True
+        return self._node1.layer - self._node2.layer < 2 and self._node1.balanced()
 
     def AABBcollision(self, rect: pygame.FRect, result: list, id) -> None:
         if self.rect.colliderect(rect):
@@ -152,6 +184,8 @@ class AABBContainer(AABBNode):
             self._node2.AABBcollision(rect, result, id + 1)
 
     def print(self, display: pygame.Surface) -> None:
-        pygame.draw.rect(display, (self._layer * 20, self._layer * 20, self._layer * 20), self._rect, 1)
+        print('node1: ', self._node1.layer)
+        print('node2: ', self._node2.layer)
+        pygame.draw.rect(display, (self._layer * 20, self._layer * 20, self._layer * 20), self._rect, 0)
         self._node1.print(display)
         self._node2.print(display)
